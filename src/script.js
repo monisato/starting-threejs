a1654c72ba7bbc0e62eb60dd12d20845c788f5ab
monisato <monicasakane@gmail.com>
2021-04-01 16:45:12 -0300

    teste 2

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
-// import * as THREE from 'three'
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import * as THREE from 'three'
+// import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
